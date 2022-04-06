e3ef1c5935917f3c88506d45aaca84447c2a5468
Jagadeesh-Reddy-Bhimireddy <101527269+Jagadeesh-Reddy-Bhimireddy@users.noreply.github.com>
2022-04-06 03:49:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return 'Hello World! This is the first python Web Application by Jagadeesh_Reddy\n'
+    return 'Hello World! This is the first python Web Application by me\n'
 if __name__ == '__main__':
     app.run()
